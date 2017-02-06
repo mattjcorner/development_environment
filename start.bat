df96e9511768f93a96b6f8558b1261a040fb320d
Matthew Corner <mcorner@payments.ca>
2017-02-06 23:06:49 +0000

    Added .gitconfig mapping from the host machine to the container

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 SET HOSTNAME=mattdev
-docker run -ti -e TERM -v /var/run/docker.sock:/var/run/docker.sock -v /c/Users/%USERNAME%/Projects:/root/Projects -v /c/Users/%USERNAME%/.ssh:/root/.ssh --hostname %HOSTNAME% mattjc/linux_dev_environment:latest /bin/bash
+docker run -ti -e TERM -v /var/run/docker.sock:/var/run/docker.sock -v /c/Users/%USERNAME%/Projects:/root/Projects -v /c/Users/%USERNAME%/.ssh:/root/.ssh -v /c/Users/%USERNAME%/.gitconfig:/root/.gitconfig --hostname %HOSTNAME% mattjc/linux_dev_environment:latest /bin/bash

82b7689375c965087b91acf442165ba5d35facf9
Matthew Corner <mcorner@payments.ca>
2017-02-07 13:30:55 -0500

    Fixed the Windows terminal problem, need to be set to cygwin but the bashrc inject was overwriting the TERM variable

--- start.bat
+++ start.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
 
 SET HOSTNAME=mattdev
-docker run -ti -e TERM -v /var/run/docker.sock:/var/run/docker.sock -v /c/Users/%USERNAME%/Projects:/root/Projects -v /c/Users/%USERNAME%/.ssh:/root/.ssh -v /c/Users/%USERNAME%/.gitconfig:/root/.gitconfig --hostname %HOSTNAME% mattjc/linux_dev_environment:latest /bin/bash
+SET TERM=cygwin
+SET ANSICON=true
+docker run -ti -e TERM=%TERM% -v /var/run/docker.sock:/var/run/docker.sock -v /c/Users/%USERNAME%/Projects:/root/Projects -v /c/Users/%USERNAME%/.ssh:/root/.ssh -v /c/Users/%USERNAME%/.gitconfig:/root/.gitconfig --hostname %HOSTNAME% mattjc/linux_dev_environment:latest /bin/bash

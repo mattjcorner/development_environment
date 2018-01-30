1fedbf82ad5608d9e54f358f3f553f7eec52d1ea
Matthew Corner <matt.corner@gmail.com>
2018-01-29 22:28:09 -0500

    Added new packages, yaml vim, mount point

--- scripts/start.bat
+++ scripts/start.bat
@@ -3,6 +3,7 @@
 SET HOSTNAME=mattdev
 SET DRIVELETTER=u
 SET USERHOME=/%DRIVELETTER%%HOMEPATH%
+SET DOWNLOADSFOLDER=/e/Users/mattjc/Downloads
 
 REM Set TERM to cygwin to enable scrolling in apps like vim
 SET TERM=cygwin
@@ -13,8 +14,9 @@ docker run -ti -e TERM=%TERM% -v /var/run/docker.sock:/var/run/docker.sock^
  -v %USERHOME:\=/%/Projects:/root/Projects^
  -v %USERHOME:\=/%/.ssh:/tmp/.ssh:ro^
  -v %USERHOME:\=/%/.gitconfig:/root/.gitconfig^
- -v %USERHOME:\=/%/.aws:/root/.aws:ro^
+ -v %USERHOME:\=/%/.aws:/root/.aws:rw^
  -v %USERHOME:\=/%/.pypirc:/root/.pypirc:ro^
  --network=host^
  --hostname %HOSTNAME%^
+ %*^
  mattjc/development_environment:latest /bin/bash

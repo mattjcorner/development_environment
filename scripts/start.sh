accdf420986ff8e252c543229bab49a3b32d044d
Matthew Corner <matt.corner@gmail.com>
2018-01-30 04:46:55 +0000

    Fix linux based distro start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,13 +2,13 @@
 
 export HOSTNAME=mattdev
 
-docker run -ti -e "DOCKER_HOST=tcp://0.0.0.0:2375" \
+docker run -ti \
+-v "/var/run/docker.sock:/var/run/docker.sock" \
 -v "/${HOME}/Projects:/root/Projects" \
 -v "/${HOME}/.ssh:/root/.ssh" \
 -v "/${HOME}/.gitconfig:/root/.gitconfig" \
 -v "/${HOME}/.aws:/root/.aws":ro \
--v "/${HOME}/.pypirc:/root/.pypirc":ro \
--v "/${HOME}/Downloads:/root/Downloads"
+-v "/${HOME}/Downloads:/root/Downloads" \
 -p 80:80 \
 -p 8080:8080 \
 --hostname "$HOSTNAME" \

6d45c8c22378f670b4d7e8caab58d34b76dfc46f
Matthew Corner <matt@papertree.io>
2018-12-03 11:52:15 -0500

    Updated windows script based on Docker changes

--- scripts/start.bat
+++ scripts/start.bat
@@ -1,21 +1,20 @@
 @ECHO OFF
 
 SET HOSTNAME=mattdev
-SET DRIVELETTER=u
-SET USERHOME=/%DRIVELETTER%%HOMEPATH%
-SET DOWNLOADSFOLDER=/e/Users/mattjc/Downloads
+SET USERHOME=%HOMEDRIVE%%HOMEPATH%
+SET DOWNLOADSFOLDER=%USERHOME%/Downloads
 
 REM Set TERM to cygwin to enable scrolling in apps like vim
 SET TERM=cygwin
 SET ANSICON=true
 
 docker run -ti -e TERM=%TERM% -v /var/run/docker.sock:/var/run/docker.sock^
- -v %USERHOME:\=/%/.docker:/root/.docker:rw^
- -v %USERHOME:\=/%/Projects:/root/Projects^
- -v %USERHOME:\=/%/.ssh:/tmp/.ssh:ro^
- -v %USERHOME:\=/%/.gitconfig:/root/.gitconfig^
- -v %USERHOME:\=/%/.aws:/root/.aws:rw^
- -v %USERHOME:\=/%/.pypirc:/root/.pypirc:ro^
+ -v %USERHOME%\.docker:/root/.docker:rw^
+ -v %USERHOME%\Projects:/root/Projects^
+ -v %USERHOME%\.ssh:/tmp/.ssh:ro^
+ -v %USERHOME%\.gitconfig:/root/.gitconfig^
+ -v %USERHOME%\.aws:/root/.aws:rw^
+ -v %USERHOME%\.pypirc:/root/.pypirc:ro^
  -p 80:80^
  -p 8001:8001^
  -p 8080:8080^
